9357a0dd32f1cbd794e48588f719a4499d31b325
Adrian Cayaco <acayaco@stratpoint.com>
2016-08-11 11:20:13 +0800

    Added ScreenMeet and SlackTextViewController.

---
+++ Remember The Date/ScreenMeetManager.m
@@ -0,0 +1,403 @@
+//
+//  ScreenMeetManager.m
+//  GServices
+//
+//  Created by Adrian Cayaco on 13/07/2016.
+//  Copyright © 2016 Stratpoint. All rights reserved.
+//
+
+#import "ScreenMeetManager.h"
+#import "AppDelegate.h"
+#import "ACTouchRecognizer.h"
+#import "SMSettingsViewController.h"
+#import "SMAccountViewController.h"
+
+#import "GGUIManager.h"
+#import "GGThemedNavigationController.h"
+#import "KYDrawerController.h"
+
+#import <ScreenMeetSDK/ScreenMeetSDK-Swift.h>
+
+
+@interface ScreenMeetManager () <UIGestureRecognizerDelegate, UIAlertViewDelegate>
+
+@property (assign, nonatomic) AppDelegate   *appDelegate;
+
+@property (strong, nonatomic) NSString      *token;
+@property (strong, nonatomic) NSString      *username;
+@property (strong, nonatomic) NSString      *password;
+
+@property (strong, nonatomic) UIAlertView   *tokenAlert;
+@property (strong, nonatomic) UIAlertView   *usernameAlert;
+
+@property (assign, nonatomic) BOOL          isProduction;
+
+@end
+
+@implementation ScreenMeetManager
+
+@synthesize appDelegate = _appDelegate;
+
+static ScreenMeetManager *manager = nil;
+
++ (ScreenMeetManager *)sharedManager
+{
+    @synchronized(self) {
+        if (!manager) {
+            manager = (ScreenMeetManager *)[[self alloc] init];
+        }
+    }
+    return manager;
+}
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+//        ACTouchRecognizer *recognizer   = [[ACTouchRecognizer alloc] init];
+//        recognizer.cancelsTouchesInView = NO;
+//        recognizer.delegate             = self;
+//        
+//        [self.appDelegate.window addGestureRecognizer:recognizer];
+        
+//        UIScreenEdgePanGestureRecognizer *swipeRecognizer = [[UIScreenEdgePanGestureRecognizer alloc] initWithTarget:self action:@selector(handleSwipeUpGesture:)];
+//        swipeRecognizer.edges                             = UIRectEdgeBottom;
+//        swipeRecognizer.delegate                          = self;
+//        
+//        [self.appDelegate.window addGestureRecognizer:swipeRecognizer];
+        
+        UITapGestureRecognizer *doubleTapGestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleDoubleTap)];
+        doubleTapGestureRecognizer.numberOfTapsRequired    = 2;
+        doubleTapGestureRecognizer.cancelsTouchesInView    = NO;
+        
+        [self.appDelegate.window addGestureRecognizer:doubleTapGestureRecognizer];
+        
+    }
+    return self;
+}
+
+#pragma mark - Accessors
+
+- (AppDelegate *)appDelegate
+{
+    if (!_appDelegate) {
+        _appDelegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
+    }
+    return _appDelegate;
+}
+
+#pragma mark - Private Methods
+
+- (void)initiateScreenMeetinProd:(BOOL)inProd
+{
+    if (inProd) {
+        [ScreenMeet initSharedInstance:@"f6b5eda921c749968fa4cd240e7fbe1c" environment:EnvironmentTypePRODUCTION];
+    } else {
+        [ScreenMeet initSharedInstance:@"19ef50c67e8648f08dfc4702f992159d" environment:EnvironmentTypeSANDBOX];
+    }
+}
+
+- (void)handleSwipeUpGesture:(UIScreenEdgePanGestureRecognizer *)gesture
+{
+    NSLog(@"SWIPE UP!!");
+}
+
+- (void)handleDoubleTap
+{
+    [self showMenu];
+}
+
+- (void)showMenu
+{
+    
+    NSString *menuTitle = @"None";
+    
+    if ([ScreenMeet sharedInstance]) {
+        if (self.isProduction) {
+            menuTitle = @"ScreenMeet (Production)";
+        } else {
+            menuTitle = @"ScreenMeet (Sandbox)";
+        }
+    }
+    
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:menuTitle message:@"" preferredStyle:UIAlertControllerStyleActionSheet];
+    
+    if ([[ScreenMeet sharedInstance] isUserLoggedIn]) {
+        UIAlertAction *logoutAction = [UIAlertAction actionWithTitle:@"Logout" style:UIAlertActionStyleDestructive handler:^(UIAlertAction * _Nonnull action) {
+            [alert dismissViewControllerAnimated:NO completion:nil];
+            
+            // actions
+            [self logout];
+        }];
+        
+        [alert addAction:logoutAction];
+        
+        switch ([[ScreenMeet sharedInstance] getStreamState]) {
+            case StreamStateTypeACTIVE: {
+                
+                UIAlertAction *stopStreamingAction = [UIAlertAction actionWithTitle:@"Stop Streaming" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+                    [alert dismissViewControllerAnimated:NO completion:nil];
+                    
+                    // actions
+                    [self stopStream];
+                }];
+                
+                
+                [alert addAction:stopStreamingAction];
+                
+                UIAlertAction *pauseStreamAction = [UIAlertAction actionWithTitle:@"Pause Streaming" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+                    [alert dismissViewControllerAnimated:NO completion:nil];
+                    
+                    // actions
+                    [self pauseStream];
+                }];
+                
+                [alert addAction:pauseStreamAction];
+                
+                break;
+            }
+            case StreamStateTypePAUSED: {
+                
+                UIAlertAction *stopStreamingAction = [UIAlertAction actionWithTitle:@"Stop Streaming" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+                    [alert dismissViewControllerAnimated:NO completion:nil];
+                    
+                    // actions
+                    [self stopStream];
+                }];
+                
+                
+                [alert addAction:stopStreamingAction];
+                
+                UIAlertAction *resumeStreamingAction = [UIAlertAction actionWithTitle:@"Resume Streaming" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+                    [alert dismissViewControllerAnimated:NO completion:nil];
+                    
+                    // actions
+                    [self resumeStream];
+                }];
+                
+                [alert addAction:resumeStreamingAction];
+                
+                break;
+            }
+            case  StreamStateTypeSTOPPED:
+            default: {
+                
+                
+                UIAlertAction *startStreamingAction = [UIAlertAction actionWithTitle:@"Start Streaming" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+                    [alert dismissViewControllerAnimated:NO completion:nil];
+                    
+                    // actions
+                    [self startStream:^(NSInteger status) {
+                        if (status == CallStatusSUCCESS) {
+                            NSLog(@"Stream started...");
+                        } else {
+                            [self showDefaultError];
+                        }
+                    }];
+                }];
+                
+                [alert addAction:startStreamingAction];
+                
+                break;
+            }
+        }
+        
+        UIAlertAction *showURLAction = [UIAlertAction actionWithTitle:@"Show URL" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+            
+            UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"" message:[[ScreenMeet sharedInstance] getRoomUrl] delegate:self cancelButtonTitle:@"Ok" otherButtonTitles:nil];
+            [alertView show];
+        }];
+        
+        [alert addAction:showURLAction];
+        
+//        UIAlertAction *accountAction = [UIAlertAction actionWithTitle:@"Account" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+//            
+//            GGThemedNavigationController *navVC = [[GGThemedNavigationController alloc] initWithRootViewController:[[SMAccountViewController alloc] init]];
+//            
+//            
+//            [[GGUIManager sharedManager] presentViewController:navVC animated:YES completion:^{
+//                
+//            }];
+//        }];
+//        
+//        [alert addAction:accountAction];
+        
+        UIAlertAction *settingsAction = [UIAlertAction actionWithTitle:@"Settings" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+            
+            GGThemedNavigationController *navVC = [[GGThemedNavigationController alloc] initWithRootViewController:[[SMSettingsViewController alloc] init]];
+            
+            
+            [[GGUIManager sharedManager] presentViewController:navVC animated:YES completion:^{
+                
+            }];
+        }];
+        
+        [alert addAction:settingsAction];
+
+    } else {
+        if ([ScreenMeet sharedInstance]) {
+            UIAlertAction *loginWithTokenAction = [UIAlertAction actionWithTitle:@"Login with Token" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+                [alert dismissViewControllerAnimated:NO completion:nil];
+                
+                self.tokenAlert = [[UIAlertView alloc] initWithTitle:@"Enter Token" message:@"" delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:@"Login",nil];
+                self.tokenAlert.alertViewStyle = UIAlertViewStylePlainTextInput;
+                
+//                UITextField *aTextField = [self.tokenAlert textFieldAtIndex:0];
+//                if (aTextField) {
+//                    aTextField.text = @"3gfl0w8eeisks8o4cwsoco4cc0owsss88wcs4ookkoccgk4844";
+//                }
+                
+                [self.tokenAlert show];
+            }];
+            
+            
+            UIAlertAction *loginWithUsernameAction = [UIAlertAction actionWithTitle:@"Login with Username" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+                [alert dismissViewControllerAnimated:NO completion:nil];
+                
+                self.usernameAlert = [[UIAlertView alloc] initWithTitle:@"Login" message:@"Please enter your username and password:" delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:@"Login", nil];
+                
+                self.usernameAlert.alertViewStyle = UIAlertViewStyleLoginAndPasswordInput;
+                
+                [self.usernameAlert show];
+            }];
+            
+            [alert addAction:loginWithTokenAction];
+            [alert addAction:loginWithUsernameAction];
+        } else {
+            UIAlertAction *chooseProductionAction = [UIAlertAction actionWithTitle:@"Production" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+                [alert dismissViewControllerAnimated:NO completion:nil];
+                
+                self.isProduction = YES;
+                
+                [self initiateScreenMeetinProd:self.isProduction];
+            }];
+            
+            
+            UIAlertAction *chooseSandboxAction = [UIAlertAction actionWithTitle:@"Sandbox" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+                [alert dismissViewControllerAnimated:NO completion:nil];
+                
+                self.isProduction = NO;
+            
+                [self initiateScreenMeetinProd:self.isProduction];
+            }];
+            
+            [alert addAction:chooseProductionAction];
+            [alert addAction:chooseSandboxAction];
+        }
+    }
+    
+    
+    UIAlertAction *exitAction = [UIAlertAction actionWithTitle:@"Exit" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+        [alert dismissViewControllerAnimated:NO completion:nil];
+    }];
+    
+    [alert addAction:exitAction];
+    
+    id rootVC = nil;
+    if ([self.appDelegate.window.rootViewController isKindOfClass:[KYDrawerController class]]) {
+        KYDrawerController *aController = (KYDrawerController *)self.appDelegate.window.rootViewController;
+        rootVC = aController.mainViewController;
+    } else {
+        rootVC = self.appDelegate.window.rootViewController;
+    }
+    
+    [rootVC presentViewController:alert animated:YES completion:^{
+        
+    }];
+}
+
+#pragma mark - Public Methods
+
+- (void)showWelcomeDialog
+{
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Welcome"
+                                                        message:[[ScreenMeet sharedInstance] getUserName]
+                                                       delegate:self
+                                              cancelButtonTitle:@"close"
+                                              otherButtonTitles:nil];
+    [alertView show];
+}
+
+- (void)showDefaultError
+{
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Error"
+                                                 message:@"Oops something went wrong"
+                                                delegate:self
+                                       cancelButtonTitle:@"Ok"
+                                       otherButtonTitles:nil];
+    [alertView show];
+}
+
+- (void)loginWithToken:(NSString *)token
+{
+    [[ScreenMeet sharedInstance] authenticate:token callback:^(enum CallStatus status) {
+        if (status == CallStatusSUCCESS) {
+            // logged in
+            [self showWelcomeDialog];
+        } else {
+            [self showDefaultError];
+        }
+    }];
+}
+
+- (void)loginWithUsername:(NSString *)username andPassword:(NSString *)password
+{
+    [[ScreenMeet sharedInstance] authenticate:username password:password callback:^(enum CallStatus status) {
+        if (status == CallStatusSUCCESS) {
+            [self showWelcomeDialog];
+        } else {
+            [self showDefaultError];
+        }
+    }];
+}
+
+- (void)logout
+{
+    [[ScreenMeet sharedInstance] logoutUser];
+}
+
+- (void)startStream:(void (^)(NSInteger status))callback
+{
+    [[ScreenMeet sharedInstance] startStream:^(enum CallStatus status) {
+        callback(status);
+    }];
+}
+
+- (void)stopStream
+{
+    [[ScreenMeet sharedInstance] stopStream];
+    NSLog(@"Stream stopped...");
+}
+
+- (void)pauseStream
+{
+    [[ScreenMeet sharedInstance] pauseStream];
+    NSLog(@"Stream paused...");
+}
+
+- (void)resumeStream
+{
+    [[ScreenMeet sharedInstance] resumeStream];
+    NSLog(@"Stream resumed...");
+}
+
+#pragma mark - UIAlertView Delegate
+
+- (void)alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex
+{
+    if (alertView == self.tokenAlert) {
+        if (buttonIndex != alertView.cancelButtonIndex) {
+            self.token = [alertView textFieldAtIndex:0].text;
+            [self loginWithToken:self.token];
+        }
+    } else if (alertView == self.usernameAlert) {
+        if (buttonIndex != alertView.cancelButtonIndex) {
+            self.username = [alertView textFieldAtIndex:0].text;
+            self.password = [alertView textFieldAtIndex:1].text;
+            
+            [self loginWithUsername:self.username andPassword:self.password];
+        }
+    }
+}
+
+@end
